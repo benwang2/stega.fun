b57651c6a4a657b781cd636d32f86a7cc21d25f1
benwang2 <eb.benwang@gmail.com>
2021-05-25 22:01:25 -0400

    Configured code, added and removed pages.
    Added more meta tags, configured canvas to fit to screen, and added alert when there is no encoded secret in an image. Configured styling.

--- resources/js/steganography.js
+++ resources/js/steganography.js
@@ -1,3 +1,76 @@
+var preview, canvas, ctx, output, fields;
+
+addLoadEvent(onPageLoaded)
+
+function onPageLoaded(){
+    var uploader = document.getElementById("file-upload")
+
+    output = document.getElementById("output");
+    canvas = document.getElementById("canvas");
+    
+    ctx = canvas.getContext('2d');
+
+    var download = document.getElementById("download")
+    var encode = document.getElementById("encode")
+    var decode = document.getElementById("decode")
+
+    var inputs = [document.getElementById("file-upload-form"),document.getElementById("options")]
+
+    fields = {}
+    var image = null;
+
+    for (const field of ["fname","message","secret","encode","decode"]){
+        fields[field] = document.getElementById(field)
+    }
+
+    function onUploadChanged(e){
+        var reader = new FileReader();
+        reader.onload = function(event){
+            image = new Image();
+            image.onload = function(){
+                canvas.width = image.width;
+                canvas.height = image.height;
+                ctx.drawImage(image,0,0);
+            }
+            image.src = event.target.result;
+
+            if (!inputs[0].classList.contains("hidden"))
+                inputs[0].classList.add("hidden")
+            if (inputs[1].classList.contains("hidden"))
+                inputs[1].classList.remove("hidden")
+
+            fields["fname"].value = e.target.files[0].name;
+        }
+        reader.readAsDataURL(e.target.files[0]); 
+    }
+
+    document.getElementById("hide-output").addEventListener("click",()=>{
+        output.classList.add("hidden")
+    })
+    document.getElementById("hide-options").addEventListener("click",()=>{
+        inputs[1].classList.add("hidden")
+        if (inputs[0].classList.contains("hidden"))
+            inputs[0].classList.remove("hidden")
+    })
+    
+    uploader.addEventListener("change", onUploadChanged, false)
+
+    encode.addEventListener("click",()=>{
+        encodeImage(image, ctx, fields["message"].value, fields["secret"].value)
+    })
+
+    decode.addEventListener("click",()=>{
+        decodeImage(image, ctx, fields["secret"].value)
+    })
+
+    var link = document.createElement('a');
+    download.addEventListener("click",()=>{
+        link.download = fields["fname"].value;
+        link.href = canvas.toDataURL()
+        link.click();
+    })
+}
+
 function toBin(item, length){
     let result = "";
     for(let i=length-1; i>=0; i--) result += (item >> i) & 1;
@@ -103,6 +176,10 @@ function decodeImage(image, ctx, secret){
     }
 
     message = CryptoJS.AES.decrypt(message, secret).toString(CryptoJS.enc.Utf8);
-    fields["message"].innerHTML = message;
-    fields["message"].value = message;
+    if (message != ""){
+        fields["message"].innerHTML = message;
+        fields["message"].value = message;
+    } else {
+        alert("No encrypted message with this secret")
+    }
 }
--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-var canvas, ctx, output, fields;
+function addLoadEvent(func) { 
+	var oldonload = window.onload; 
+	if (typeof window.onload != 'function') { 
+		window.onload = func; 
+	} else { 
+		window.onload = function() { 
+			if (oldonload) { 
+				oldonload(); 
+			} 
+			func(); 
+		} 
+	} 
+} 
 
-window.addEventListener("load",onPageLoaded)
-
-function onPageLoaded(){
+function setupNav(){
     var hamburger = document.getElementById("hamburger")
     var sidebar = document.querySelector(".sidebar")
-    var uploader = document.getElementById("file-upload")
-
-    output = document.getElementById("output");
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext('2d');
-
-    var download = document.getElementById("download")
-    var encode = document.getElementById("encode")
-    var decode = document.getElementById("decode")
-
-    var inputs = [document.getElementById("file-upload-form"),document.getElementById("options")]
-
-    fields = {}
-    var image = null;
-
-    for (const field of ["fname","message","secret","encode","decode"]){
-        fields[field] = document.getElementById(field)
-    }
 
     function toggleMenu(){
         if (hamburger.classList.contains("is-active")){
@@ -34,50 +26,8 @@ function onPageLoaded(){
         }
     }
 
-    function onUploadChanged(e){
-        var reader = new FileReader();
-        reader.onload = function(event){
-            image = new Image();
-            image.onload = function(){
-                canvas.width = image.width;
-                canvas.height = image.height;
-                ctx.drawImage(image,0,0);
-            }
-            image.src = event.target.result;
-
-            if (!inputs[0].classList.contains("hidden"))
-                inputs[0].classList.add("hidden")
-            if (inputs[1].classList.contains("hidden"))
-                inputs[1].classList.remove("hidden")
-
-            fields["fname"].value = e.target.files[0].name;
-        }
-        reader.readAsDataURL(e.target.files[0]); 
-    }
-
-    document.getElementById("hide-output").addEventListener("click",()=>{
-        output.classList.add("hidden")
-    })
-    document.getElementById("hide-options").addEventListener("click",()=>{
-        inputs[1].classList.add("hidden")
-        if (inputs[0].classList.contains("hidden"))
-            inputs[0].classList.remove("hidden")
-    })
     hamburger.addEventListener("click", toggleMenu)
-    uploader.addEventListener("change", onUploadChanged, false)
-
-    encode.addEventListener("click",()=>{
-        encodeImage(image, ctx, fields["message"].value, fields["secret"].value)
-    })
-
-    decode.addEventListener("click",()=>{
-        decodeImage(image, ctx, fields["secret"].value)
-    })
-
-    var link = document.createElement('a');
-    download.addEventListener("click",()=>{
-        link.download = fields["fname"].value;
-        link.href = canvas.toDataURL()
-        link.click();
-    })
+    console.log("page loaded")
 }
+
+addLoadEvent(setupNav)
